fd2981af9722594cd5c0d68f3ee3a7a89fdd7d5f
jyferreira <121454300+jyferreira@users.noreply.github.com>
2023-01-12 20:02:41 +0000

    Atualização pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(90)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
 })
