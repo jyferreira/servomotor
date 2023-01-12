be064df013da56df64b1aadfcfdae96bc7eb40bb
jyferreira <121454300+jyferreira@users.noreply.github.com>
2023-01-12 20:16:25 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,31 @@ input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(90)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
 })
 pins.digitalWritePin(DigitalPin.P2, 1)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
